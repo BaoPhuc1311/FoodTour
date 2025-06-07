1c3e9b31f7c47398681bd11694b017950fd06ca9
Bao Phuc <lhbaophuc0802@gmail.com>
2025-06-07 22:40:28 +0700

    Initialize Food Tour project

---
+++ src/main/java/com/foodtour/FoodTourApplication.java
@@ -0,0 +1,13 @@
+package com.foodtour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodtour/FoodTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodtour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
